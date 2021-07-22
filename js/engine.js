2e8df7837a0ad44571dd309e3373ca816f33de05
Nobody5050 <levibelland@gmail.com>
2021-07-22 16:50:29 -0500

    changes i guess

--- js/engine.js
+++ js/engine.js
@@ -1,6 +1,6 @@
 function draw() {
-   var canvas = document.getElementById('tutorial');
+   var canvas = document.getElementById('canvas');
    if (canvas.getContext) {
       var ctx = canvas.getContext('2d');
-   }
+  }
 }
